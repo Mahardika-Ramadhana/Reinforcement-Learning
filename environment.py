48995c69dea0544671541e65925150ef61e2769c
Dika Hacker <aku@hacker.com>
2026-04-25 22:21:25 +0700

    adding apple to the environment

--- environment.py
+++ environment.py
@@ -1,22 +1,26 @@
 import pygame
 import sys
 import numpy as np
+import random
 
 class SnakeEnv:
     def __init__(self):
         pygame.init()
         self.window_size = 800
-        self.cell_size = 40
+        self.cell_size = 100
         self.screen = pygame.display.set_mode((self.window_size, self.window_size))
         pygame.display.set_caption("RL Playground - Snake")
         self.clock = pygame.time.Clock()
 
         self.snake_pos = [400, 400]
         self.done = False
+        self._place_food()
 
     def reset(self):
         self.snake_pos = [400, 400]
         self.done = False
+        self._place_food()
+
         return self._get_state()
 
     def step(self, action):
@@ -27,10 +31,20 @@ class SnakeEnv:
 
         if action == 0: self.snake_pos[1] -= self.cell_size    # Atas 
         elif action == 1: self.snake_pos[0] += self.cell_size  # Kanan
-        elif action == 2: self.snake_pos[1] += self.cell_size  # Bawah.
+        elif action == 2: self.snake_pos[1] += self.cell_size  # Bawah
         elif action == 3: self.snake_pos[0] -= self.cell_size  # Kiri
 
         reward = 0
+
+        if self.snake_pos[0] == self.food_pos[0] and self.snake_pos[1] == self.food_pos[1]:
+            print("Ular makan apel di koordinat:", self.food_pos)
+            reward = 10             
+            self._place_food()
+
+        if self.snake_pos[0] == self.food_pos[0] and self.snake_pos[1] == self.food_pos[1]:
+            reward = 10
+            self._place_food
+
         if (self.snake_pos[0] < 0 or self.snake_pos[0] >= self.window_size or
             self.snake_pos[1] < 0 or self.snake_pos[1] >= self.window_size):
             self.done = True
@@ -40,13 +54,24 @@ class SnakeEnv:
 
     def render(self):
         self.screen.fill((0, 0, 0)) # Black background
+
         pygame.draw.rect(self.screen, (0, 255, 0),
                         (self.snake_pos[0], self.snake_pos[1], self.cell_size, self.cell_size))
+
+        pygame.draw.rect(self.screen, (255, 0, 0), 
+                         (self.food_pos[0], self.food_pos[1], self.cell_size, self.cell_size))
+
         pygame.display.flip()
         self.clock.tick(10)
 
     def _get_state(self):
         return np.array(self.snake_pos)
+
+    def _place_food(self):
+        x = random.randint(0, (self.window_size // self.cell_size) -1) * self.cell_size
+        y = random.randint(0, (self.window_size // self.cell_size) -1) * self.cell_size 
+
+        self.food_pos = [x, y]
 
 if __name__ == "__main__":
     env = SnakeEnv()
