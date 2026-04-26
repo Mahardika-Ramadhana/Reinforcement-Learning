7710edcd89b924bc6137a0fd12453a5e017673b6
Dika Hacker <aku@hacker.com>
2026-04-26 22:16:19 +0700

    fix: syntax error in model.py

--- agent.py
+++ agent.py
@@ -2,4 +2,23 @@ import torch
 import random
 import numpy as np
 from collections import deque
-from model import 
+from environment import SnakeEnv
+from model import Linear_QNet, QTrainer
+
+# Parameter Global
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+
+class Agent:
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0  # randomness
+        self.gamma = 0.9  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)
+
+        # Inisialisasi Model & Trainer
+        # Input: 11 (State), Hidden: 256, Output: 3 (Action)
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
+import numpy as np
 
 
 class Linear_QNet(nn.Module):
@@ -16,6 +17,13 @@ class Linear_QNet(nn.Module):
         x = self.linear2(x)
         return x
 
+    def save(self, file_name="model.pth"):
+        model_folder_path = "./model"
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
 
 class QTrainer:
     def __init__(self, model, lr, gamma):
@@ -23,7 +31,7 @@ class QTrainer:
         self.gamma = gamma
         self.model = model
         self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
-        self, criterion == nn.MSELoss()
+        self.criterion = nn.MSELoss()
 
     def train_step(self, state, action, reward, next_state, done):
         state = torch.tensor(np.array(state), dtype=torch.float)
